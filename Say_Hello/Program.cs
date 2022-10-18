e5366bd3ea4c758c2f451dfd7db54fc4698fac12
carolinehanaa <carolinehana28@gmail.com>
2022-10-18 15:21:57 -0700

    First Push

---
+++ Say_Hello/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+//10-18-2022
+//Caroline Hana
+//Mini Challenge #1 "What is your name?"
+//This is console project that the users input and print their name
+//peer review;
+
+Console.Clear();
+
+string? playAgain = "yes";
+while (playAgain == "yes")
+{
+    string? myName;
+
+    Console.WriteLine("Hello, what is your name?");
+    myName = Console.ReadLine();
+
+    Console.WriteLine($"Hello {myName}");
+
+    string? answer = "";
+    while (answer != "yes" && answer != "no")
+    {
+        Console.WriteLine("Would you like to enter another name? (yes or no)");
+        answer = Console.ReadLine();
+        answer = answer.ToLower();
+             if(answer == "yes")
+             {
+                playAgain = answer;
+             }
+             else if (answer == "no")
+             {
+                playAgain = answer;
+             }
+             else
+             {
+                Console.WriteLine("Invalid Answer");
+             }
+
+    }
+
+
+
+
+
+
+
+}
