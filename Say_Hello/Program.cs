c5b63154e18e71302f26276f83e188468a735f20
carolinehanaa <carolinehana28@gmail.com>
2022-10-20 11:39:22 -0700

    push 2

--- Say_Hello/Program.cs
+++ Say_Hello/Program.cs
@@ -1,9 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-//10-18-2022
-//Caroline Hana
-//Mini Challenge #1 "What is your name?"
-//This is console project that the users input and print their name
-//peer review;
+﻿
 
 Console.Clear();
 
